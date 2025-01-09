2ae10ff06f2aadd078125a49316445728f1c05f4
GGvibes <grherzog@gmail.com>
2025-01-09 11:36:32 -0600

    functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import './components/Authenticate'
+import Authenticate from './components/Authenticate'
+import './components/SignUpForm'
+import SignUpForm from './components/SignUpForm'
+import { useState } from 'react'
 
 function App() {
-
+  const [token, setToken] = useState(null);
   return (
     <>
       <div>
-        
+        <Authenticate token={token} setToken={setToken}/>
+        <SignUpForm token={token} setToken ={setToken} />
       </div>
     </>
   )
---
+++ src/components/Authenticate.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+export default function Authenticate({token}) {
+
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null)
+
+async function handleClick(e) {
+    e.preventDefault;
+    
+    try {
+        const response = await fetch(
+            "https://fsa-jwt-practice.herokuapp.com/authenticate",
+              {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${token}`,
+                }   
+               }
+               
+            );
+            console.log(token)
+            const result = await response.json();
+            setSuccessMessage(result.message)
+    } catch(error) {
+        setError(error.message)
+    }
+}
+
+    return (
+        <>
+        
+        <h2>Authenticate</h2>
+        {error && <p>{error}</p>}
+        {successMessage && <p>{successMessage}</p>}
+        <button onClick={handleClick}>Authenticate Token</button>
+        
+        </>
+    )
+}
